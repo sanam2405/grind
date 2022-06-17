80d1203adec341f7dda743b443ee59b0e4f7bae7
Manas Pratim Biswas <75798265+sanam2405@users.noreply.github.com>
2022-06-17 12:56:41 -0400

    Create KthPermutationSequence.cpp

---
+++ KthPermutationSequence.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+string kthPermutation(int n, int k) {
+    
+    vector<int> numbers;
+    int fact = 1;
+    for(int i = 1; i < n; i++)
+    {
+        fact*=i;
+        numbers.push_back(i);
+    }
+    
+    numbers.push_back(n);
+    string ans;
+    k--;
+    
+    while(true)
+    {
+        ans+= to_string(numbers[k/fact]);
+        numbers.erase(numbers.begin() + k/fact);
+        
+        if(numbers.size()==0)
+            break;
+        
+        k%=fact;
+        fact/=numbers.size();
+    }
+    
+    return ans;
+}
