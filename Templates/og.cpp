40496a6d1f45d6c79c8a2f383a6a65d8f66c78f0
Manas Pratim Biwas <manaspb2405@gmail.com>
2023-11-17 10:58:23 +0530

    templates brewing starts

---
+++ Templates/og.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+
+void solve()
+{
+        
+}
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin);
+     freopen("output.txt", "w", stdout);
+#endif
+
+     ios_base::sync_with_stdio(false);
+     cin.tie(nullptr);
+     cout.tie(nullptr);
+
+     ll t;
+     t = 1;
+     // cin >> t;
+     while (t--)
+     {
+          solve();
+     }
+
+     return 0;
+}
